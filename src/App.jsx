18ec45778b5d64eb698137935e906e4d2714c0ec
Murilo0123 <murilomsilva06@gmail.com>
2024-06-08 19:12:15 +0000

    style: criação dos estilos globais

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { Home } from "./pages/Home"
 import { ThemeProvider } from "styled-components"
 import { theme } from "./styles/theme"
+import { Globalstyle } from "./styles/global"
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,7 @@ export function App() {
   return (
     <ThemeProvider theme={theme}>
       <RouterProvider router={router} />
+      <Globalstyle />
     </ThemeProvider>
   )
 }
